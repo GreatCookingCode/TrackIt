3a8ebed9a7ac368c3fd7b6c7ae33fc3bbed6652b
Karim Matrah <karim.matrah@wifirst.fr>
2013-10-01 21:14:15 +0200

    fetch tasks from api

--- trackit-web/app/scripts/controllers/home.js
+++ trackit-web/app/scripts/controllers/home.js
@@ -2,24 +2,6 @@
 
 angular.module('trackitWebApp.controllers')
     .controller('HomeCtrl', function ($scope, Task) {
-        $scope.tasks = [
-			{
-				title: 'Installer NodeJS',
-				status: 'OK',
-				body: 'blabablablablabablablablabablabla',
-
-			},
-			{
-				title: 'Installer Sails',
-				status: 'OK',
-				body: 'blabablablablabablablablabablabla',
-
-			},
-			{
-				title: 'Installer Angular',
-				status: 'OK',
-				body: 'blabablablablabablablablabablabla',
-			}
-		]
+        $scope.tasks = Task.query();
     })
 ;
