9a9deab60b58c609c75ffbdd0ceee6f91ec7166b
Karim Matrah <karim.matrah@wifirst.fr>
2013-10-01 21:41:12 +0200

    fetch todo, doing and done tasks from api

--- trackit-web/app/scripts/controllers/home.js
+++ trackit-web/app/scripts/controllers/home.js
@@ -2,11 +2,9 @@
 
 angular.module('trackitWebApp.controllers')
     .controller('HomeCtrl', function ($scope, Task) {
-        $scope.todoTasks = Task.query();
-        $scope.doingTasks = Task.query();
-        $scope.doneTasks = Task.query();
-
-        $scope.task = {};
+        $scope.todoTasks = Task.query({status: 'todo'});
+        $scope.doingTasks = Task.query({status: 'doing'});
+        $scope.doneTasks = Task.query({status: 'done'});
 
         $scope.add = function() {
             $scope.task.save({})
