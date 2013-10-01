c4eaedaa0289320cb822589601ec4b9b51cffa2c
Olivier Leplus <olivier@MacBook-Pro-de-Olivier.local>
2013-10-01 21:15:47 +0200

    Possibility to retrieve tasks by status
    call /tasks?status=Todo to retrieve only tasks with status "Todo"

--- trackit-backend/api/controllers/TasksController.js
+++ trackit-backend/api/controllers/TasksController.js
@@ -18,5 +18,26 @@
  				res.send(tasks);
  			}
  		});
+ 	},
+
+ 	index: function(req, res) {
+ 		if (req.param("status")) {
+ 			Tasks.findByStatus(req.param("status")).done(function(err, tasks) {
+ 				if (err) {
+ 					return console.log(err);} else {
+ 						res.send(tasks);
+ 					}
+ 				});
+ 		}
+ 		else {
+ 			Tasks.find().done(function(err, tasks) {
+ 				if (err) {
+ 					return console.log(err);
+ 				// res.send(500, {error: 'DB Error'});
+ 			} else {
+ 				res.send(tasks);
+ 			}
+ 		});
+ 		}
  	}
  };
