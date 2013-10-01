3d7371054eb9d824347214de869b05dfe3d35f80
blacroix <benjlacroix@gmail.com>
2013-10-01 21:17:48 +0200

    home page

--- trackit-web/app/scripts/controllers/home.js
+++ trackit-web/app/scripts/controllers/home.js
@@ -2,6 +2,8 @@
 
 angular.module('trackitWebApp.controllers')
     .controller('HomeCtrl', function ($scope, Task) {
-        $scope.tasks = Task.query();
+        $scope.todoTasks = Task.query();
+        $scope.doingTasks = Task.query();
+        $scope.doneTasks = Task.query();
     })
 ;
